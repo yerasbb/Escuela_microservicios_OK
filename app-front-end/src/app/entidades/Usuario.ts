3ada68ee901a762c82c5ad6d82f5f703f4aa4873
yeraybb <yeraybajob@gmail.com>
2019-12-20 17:20:21 +0100

    Proyecto base de AngularJS

---
+++ app-front-end/src/app/entidades/Usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario{
+    private id: number;
+    private nombre: string;
+    private email: string;
+    private password: string;
+    private temaPreferido: number;
+}
---
+++ app-front-end/src/app/usuarios.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from './entidades/Usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  listaUsuario: Usuario[];
+  
+  constructor() { }
+}
