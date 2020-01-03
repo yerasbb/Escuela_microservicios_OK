ce7ba127274d2e5eb4514fe0805dd7755899b2a6
yeraybb <yeraybajob@gmail.com>
2020-01-03 13:55:36 +0100

    Ejemplo app Angular

---
+++ app-vacia/src/app/app.componente.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-comp-raiz',
+    template: '<h2>Aquí estoy</h2>'
+})
+
+export /*equivalente a public*/ class AppComponente {
+
+}
